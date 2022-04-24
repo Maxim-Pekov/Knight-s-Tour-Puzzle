1465f7ba2fef244b5e7e442a1693798787225e5f
Maxim-Pekov <max.pekov@gmail.com>
2022-04-24 23:02:34 +0300

    change main.py, correct number of rows if it more them 10

--- main.py
+++ main.py
@@ -2,29 +2,37 @@ def starting_position(positions_a, positions_b, m):
     int(positions_a), int(positions_b)
     assert float(positions_a) > 0 and float(positions_b) > 0, "а и б должно быть больше 0"
 
-    m[len(m) - int(positions_b)][int(positions_a) - 1] = "X"
-    print(" " + "-" * 2 * len(m[0]) + "---")
+    m[len(m) - int(positions_b)][int(positions_a) - 1] = "X" if len(m) < 10 else " x"
+
+        # add "-----------------" over the table
+    print(" ---" + "-" * 2 * len(m[0])) if len(m) < 10 else print("  ---" + "-" * 3 * len(m[0]))
+
     x = len(m)
     line_number = []
     for i in range(int(len(m))):
-        print(f'{x}| {" ".join(m[i])} |')
+        print(f' {x}| {" ".join(m[i])} |') if x < 10 and len(m) > 9 else print(f'{x}| {" ".join(m[i])} |')
         x -= 1
+
     for i in range(len(m[0])):
-        line_number.append(str(i + 1))
-    print(" " + "-" * 2 * len(m[0]) + "---")
+
+        # add line "01 02 03 04..." under the table
+        line_number.append(" " + str(i + 1)) if len(m) >= 10 and i < 9 else line_number.append(str(i + 1))
+
+        # add "-----------------" under the table
+    print(" ---" + "-" * 2 * len(m[0])) if len(m) < 10 else print("  ---" + "-" * 3 * len(m[0]))
+
     z = " ".join(line_number)
-    print(f"   {z} ")
+    print(f"   {z} ") if len(m) < 10 else print(f"    {z} ")
 
 
 def starting_board(board_a, board_b):
-    assert int(board_a) < 100 or int(board_b) < 100         # check board size (1 - 99)
+    assert int(board_a) < 100 and int(board_b) < 100  # check board size (1 - 99)
     assert int(board_a) > 0 and int(board_b) > 0
     m = []
     for i in range(int(board_b)):
         m.append([])
         for j in range(int(board_a)):
-
-            m[i].append("_") if board_b < 10 and board_a < 10 else m[i].append("__")
+            m[i].append("_") if int(board_b) < 10 and int(board_a) < 10 else m[i].append("__")
     return m
 
 
@@ -51,4 +59,4 @@ def ask_first_position():
         ask_first_position()
 
 
-y = ask_first_position()
+ask_first_position()
